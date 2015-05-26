4c9d18005adc9fbb64d6ba58872094518e0f6d41
Mainak Jas <mainakjas@gmail.com>
2015-05-26 18:28:47 +0200

    ENH: add script to count examples

---
+++ count_examples.py
@@ -0,0 +1,26 @@
+"""Find number of examples in MNE-Python.
+"""
+
+# Author: Mainak Jas <mainak.jas@telecom-paristech.fr>
+
+import os
+import os.path as op
+import fnmatch
+
+from mne.datasets import sample
+
+
+def recursive_search(path, pattern):
+    """Auxiliary function for recursive_search of the directory.
+    """
+    filtered_files = list()
+    for dirpath, dirnames, files in os.walk(path):
+        for f in fnmatch.filter(files, pattern):
+            filtered_files.append(op.realpath(op.join(dirpath, f)))
+
+    return filtered_files
+
+# assuming sample data is in examples dir
+example_path = op.dirname(sample.data_path())
+example_files = recursive_search(example_path, '*.py')
+print('Number of examples is %d' % len(example_files))
